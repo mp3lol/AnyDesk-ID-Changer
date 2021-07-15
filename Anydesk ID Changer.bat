81c865c710fcb3c013fc54b751986ba2915fcb54
mp3 <85530830+mp3lol@users.noreply.github.com>
2021-07-14 19:07:32 -0500

    Update Anydesk ID Changer.bat

--- Anydesk ID Changer.bat
+++ Anydesk ID Changer.bat
@@ -3,7 +3,7 @@ IF not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /B)
 IF not exist "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" echo [-] Anydesk is not installed&timeout /t 99999 /nobreak>NUL
 IF not exist "C:\ProgramData\AnyDesk\service.conf" echo [-] Open AnyDesk and try again&timeout /t 99999 /nobreak>NUL
 IF not exist "C:\ProgramData\AnyDesk\system.conf" echo [-] Open AnyDesk and try again&timeout /t 99999 /nobreak>NUL
-tasklist /fi "ImageName eq AnyDesk.exe" /FO csv 2>nul | find /I "AnyDesk.exe">NUL 2>&1
+tasklist /fi "ImageName eq AnyDesk.exe" /FO csv 2>NUL | find /I "AnyDesk.exe">NUL 2>&1
 IF "%ERRORLEVEL%"=="0" taskkill /F /T /IM AnyDesk.exe>NUL 2>&1
 echo [*] Terminating AnyDesk
 <"C:\ProgramData\AnyDesk\system.conf" find "ad.anynet.id=">"%temp%\anydesk.txt"
@@ -17,4 +17,4 @@ del /F /Q "C:\ProgramData\AnyDesk\system.conf">NUL 2>&1
 del /F /Q "C:\ProgramData\AnyDesk\service.conf">NUL 2>&1
 del /F /Q "%anydesk%">NUL 2>&1
 echo [*] Cleaning Junk
-echo [+] Succesfully changed AnyDesk ID&start C:\"Program Files (x86)"\AnyDesk\AnyDesk.exe&timeout /t 10 /nobreak>NUL&exit /b
+echo [+] Succesfully changed AnyDesk ID&start C:\"Program Files (x86)"\AnyDesk\AnyDesk.exe&timeout /t 10 /nobreak>NUL&exit /B
